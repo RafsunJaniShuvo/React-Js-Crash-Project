34beaeadc8d533bfcff968cfbd94078e245c9aec
rafsun <rafsunjani@ba-systems.com>
2024-03-02 13:42:38 +0600

    learned props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Tittle from './Tittle';
+
+
 
 function App() {
+  const name = "Rafsun Jani shuvo 1" ;
+  let instructor = "Rafsunnnnn Fortis Group"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {name}
+      {/* props */}
+      <Tittle instructor={instructor}/>
     </div>
   );
 }
