dee98871b9b2bf1f90b4e98466984561232d3451
rafsun <rafsunjani@ba-systems.com>
2024-03-02 14:18:01 +0600

    learned useState in react js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 
+import { useState } from 'react';
 import './App.css';
 import Tittle from './Tittle';
 
 
-
 function App() {
-  const name = "Rafsun Jani shuvo 1" ;
-  let instructor = "Rafsunnnnn Fortis Group"
+  const [name,setName] = useState(" Online");
+
+  const [number,setNumber] = useState(1);
+  // const name = "Rafsun";
+  let instructor = "Rafsunnnnn Fortis Group";
+
+  const handleClick = () => {
+    setNumber(prevState => prevState + 1);
+  }
+
   return (
     <div className="App">
-      {name}
+      <h2>{number}</h2>
+        <button onClick={handleClick}>Increase Number</button>
+      <div>
+        Learning React js From {name}
+      </div>
+        <button onClick={(e)=>setName("Youtube")}>
+          click here
+        </button>
       {/* props */}
-      <Tittle instructor={instructor}/>
+      <Tittle instructor={instructor} />
     </div>
   );
 }
